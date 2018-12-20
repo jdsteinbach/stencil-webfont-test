7747f29c27190af7f8b35a18370b330073683b57
James Steinbach <jdsteinbach@gmail.com>
2018-12-20 15:58:27 -0700

    initial commit

---
+++ src/components/use-font/use-font.tsx
@@ -0,0 +1,16 @@
+import { Component } from '@stencil/core'
+
+@Component({
+  tag: 'use-font',
+  styleUrl: 'use-font.css',
+  shadow: true
+})
+
+export class UseFont {
+  render() {
+    return [
+      <p>If you see a monospace font, it’s failed.</p>,
+      <p>If you see a chunky display font, it’s working.</p>
+    ]
+  }
+}
